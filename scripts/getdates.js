935badb8d8873f990644ae7f3f64653ac93bb346
learsiml-afk <learsiml@gmail.com>
2026-05-09 04:58:31 -0700

    Finale

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").innerHTML = currentYear;
+
+
+document.getElementById("lastModified").innerHTML = "Last Modification: " + document.lastModified;
