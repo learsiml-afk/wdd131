9f6b6d6ab06de642617ab8a0b727a2d4889470bd
learsiml-afk <learsiml@gmail.com>
2026-05-13 20:48:54 -0700

    Finbom

---
+++ week02/scripts/Bom.js
@@ -0,0 +1,14 @@
+g
+        
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('______');
+
+const li = document.createElement('li');
+
+const deleteButton = document.createElement('button');
+
+li.textContent = input.value;
+deleteButton.textContent = '❌';
+li.append(deleteButton);
+list.append(li);
