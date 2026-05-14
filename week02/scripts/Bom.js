a62e82b83472dc790916546c49d76f5ebc5f15c3
learsiml-afk <learsiml@gmail.com>
2026-05-13 20:53:10 -0700

    js

--- week02/scripts/Bom.js
+++ week02/scripts/Bom.js
@@ -1,14 +1,20 @@
-g
-        
-const input = document.querySelector('#favchap');
-const button = document.querySelector('button');
-const list = document.querySelector('______');
+const inputElement = document.querySelector("#favchap");
+const buttonElement = document.querySelector("button");
+const listElement = document.querySelector("#list");
 
-const li = document.createElement('li');
-
-const deleteButton = document.createElement('button');
-
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+buttonElement.addEventListener("click", function () {
+	if (inputElement.value != "") {
+		const li = document.createElement("li");
+		li.textContent = inputElement.value;
+		const deleteBtn = document.createElement("button");
+		deleteBtn.textContent = "❌";
+		deleteBtn.addEventListener("click", function () {
+			listElement.removeChild(li);
+			inputElement.focus();
+		});
+		li.appendChild(deleteBtn);
+		listElement.appendChild(li);
+		inputElement.value = "";
+	}
+	inputElement.focus();
+});
